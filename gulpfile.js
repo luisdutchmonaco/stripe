fa57513fec2d929cf0fa6bd5976e9cd3fef01059
Luis Cruz <kfna@Luiss-iMac.local>
2021-01-15 19:13:28 -0600

    update

--- gulpfile.js
+++ gulpfile.js
@@ -11,17 +11,13 @@ function assets(cb) {
   gulp.src('src/js/**/*').pipe(gulp.dest("dist/js"));
   gulp.src('src/fonts/**/*').pipe(gulp.dest("dist/fonts"));
   gulp.src('src/**/*.{png,jpg,jpeg,svg,ico}').pipe(gulp.dest('dist/'));
-
   cb();
 }
 
-function build(cb,json) {
+function build(slug,json) {
   var templateData = json,
   options = {
-      ignorePartials: true, //ignores the unknown footer2 partial in the handlebars template, defaults to false
-      partials : {
-          footer : '<footer>the end</footer>'
-      },
+      ignorePartials: true,
       batch : ['./src/partials'],
       helpers : {
           debug : function(optionalValue){
@@ -37,32 +33,51 @@ function build(cb,json) {
           }
       }
   }
-  gulp.src('src/index.hbr').pipe(handlebars(templateData, options)).pipe(rename('index.html')).pipe(gulp.dest('dist'));
-  gulp.src('src/wow-i-can-see-clearly-now.hbr').pipe(handlebars(templateData, options)).pipe(rename('wow-i-can-see-clearly-now.html')).pipe(gulp.dest('dist'));
-  gulp.src('src/ahora-veo-todo-perfecto.hbr').pipe(handlebars(templateData, options)).pipe(rename('ahora-veo-todo-perfecto.html')).pipe(gulp.dest('dist'));
-  cb();
+  /* PAGE */
+  switch(slug){
+    case "index":
+      gulp.src('src/index.hbs').pipe(handlebars(templateData, options)).pipe(rename('index.html')).pipe(gulp.dest('dist'));
+    break;
+    case "wow-i-can-see-clearly-now":
+    case "ahora-veo-todo-perfecto.hbs":
+      gulp.src('src/wow-i-can-see-clearly-now.hbs').pipe(handlebars(templateData, options)).pipe(rename('wow-i-can-see-clearly-now.html')).pipe(gulp.dest('dist'));
+      gulp.src('src/ahora-veo-todo-perfecto.hbs').pipe(handlebars(templateData, options)).pipe(rename('ahora-veo-todo-perfecto.html')).pipe(gulp.dest('dist'));
+    break;
+  }
+  //cb();
 }
 
 
-function uielements(cb){
-  cb();
-}
-
-function data(cb){
-  var json = {};
-  const airtable = new Airtable({ apiKey: 'keyh4ZnksjZg4USrF' }).base('appuoshptISMGIo0F');
-
-
-  airtable.table('wow-i-can-see-clearly-now').list({maxRecords:100}).then(result => {
-
+async function data(cb){
+  airtable = new Airtable({ apiKey: 'keyh4ZnksjZg4USrF' }).base('appuoshptISMGIo0F');
+  /* Global Data */
+  const elements = airtable.table('UI Elements').list({maxRecords:100}).then(result => {
     var content = result.records;
-    /* structure */
+    var footer = {
+      copy : { en: _.findWhere(content, {id: "recBKcdZwWLE9X32B"}).fields.en, es: _.findWhere(content, {id: "recBKcdZwWLE9X32B"}).fields.es },
+      ebcopy : { en: _.findWhere(content, {id: "recfODyniv2R89Ewa"}).fields.en, es: _.findWhere(content, {id: "recfODyniv2R89Ewa"}).fields.es },
+      input_ph : { en: _.findWhere(content, {id: "recktzWmoVPg9c2w4"}).fields.en, es: _.findWhere(content, {id: "recktzWmoVPg9c2w4"}).fields.es },
+      input_btn : { en: _.findWhere(content, {id: "recwmP0oAmk7CB8VB"}).fields.en, es: _.findWhere(content, {id: "recwmP0oAmk7CB8VB"}).fields.es },
+    }
+    var modal = {
+      dd_title : { en: _.findWhere(content, {id: "recNctbfNoCrpOiZv"}).fields.en, es: _.findWhere(content, {id: "recNctbfNoCrpOiZv"}).fields.es },
+      dd_btncopy : { en: _.findWhere(content, {id: "recbcKCZlEKMu2Vf1"}).fields.en, es: _.findWhere(content, {id: "recbcKCZlEKMu2Vf1"}).fields.es },
+    }
+    var data = { footer: footer, modal: modal };
+    return data;
+  });
+  /* Page */
+  const page_wicscn = airtable.table('wow-i-can-see-clearly-now').list({maxRecords:100}).then(result => {
+    var content = result.records;
     var sections = {};
     var meta = {
       title : { en: _.findWhere(content, {id: "reco4WfhYUpEetkd3"}).fields.en, es: _.findWhere(content, {id: "reco4WfhYUpEetkd3"}).fields.es },
       description : { en: _.findWhere(content, {id: "recpcUy8mPO1NJ2ot"}).fields.en, es: _.findWhere(content, {id: "recpcUy8mPO1NJ2ot"}).fields.es },
       og_image : { en: _.findWhere(content, {id: "recJC2B5AfGd2vK5W"}).fields.en, es: _.findWhere(content, {id: "recJC2B5AfGd2vK5W"}).fields.es },
       og_url : { en: _.findWhere(content, {id: "recJC2B5AfGd2vK5W"}).fields.en, es: _.findWhere(content, {id: "recJC2B5AfGd2vK5W"}).fields.es },
+      shopifylink: { en: _.findWhere(content, {id: "reczCCpYmolJvjxdz"}).fields.en, es: _.findWhere(content, {id: "reczCCpYmolJvjxdz"}).fields.es },
+      shopifybtnlbl: { en: _.findWhere(content, {id: "recTnwj7lZF9SmSeT"}).fields.en, es: _.findWhere(content, {id: "recTnwj7lZF9SmSeT"}).fields.es },
+
     }
 
     buttons = {
@@ -79,21 +94,9 @@ function data(cb){
       badge_bottom : { en: _.findWhere(content, {id: "rec2cMcusHXXRqSwQ"}).fields.en, es: _.findWhere(content, {id: "rec2cMcusHXXRqSwQ"}).fields.es },
     }
 
-    footer = { };
-/*
-    footer = {
-      copy : { en: _.findWhere(content, {id: "recDbeDyjfn6uLT4D"}).fields.en, es: _.findWhere(content, {id: "recDbeDyjfn6uLT4D"}).fields.es },
-      subscribe_btn_lbl : { en: _.findWhere(content, {id: "recN0faHaMPP1ArDv"}).fields.en, es: _.findWhere(content, {id: "recN0faHaMPP1ArDv"}).fields.es },
-      subscribe_btn_pholder : { en: _.findWhere(content, {id: "recTZinn3WHv38rAS"}).fields.en, es: _.findWhere(content, {id: "recTZinn3WHv38rAS"}).fields.es },
-    }
-    */
-
     var attachments = _.findWhere(content, {id: "rec4XnNGwnCtdXWbA"}).fields.Attachments;
     var pdfs = _.findWhere(content, {id: "rec4XnNGwnCtdXWbA"}).fields.Files;
-    attachments.forEach(function(item,index){
-      attachments[index].pdf = pdfs[index].url;
-    });
-
+    attachments.forEach(function(item,index){ attachments[index].pdf = pdfs[index].url; });
 
     sections = {
       ourbook : {
@@ -115,9 +118,40 @@ function data(cb){
       }
     }
 
-    var data = { meta : meta, hero : hero, sections: sections, footer: footer, buttons: buttons };
-    build(function(){},data);
+    footer = {
+      amazon: { en: _.findWhere(content, {id: "recyI7E4wI93zhr7a"}).fields.en, es: _.findWhere(content, {id: "recyI7E4wI93zhr7a"}).fields.es },
+      barnesandnoble: { en: _.findWhere(content, {id: "recQD3zDWa9T6NsWZ"}).fields.en, es: _.findWhere(content, {id: "recQD3zDWa9T6NsWZ"}).fields.es },
+      googleplay: { en: _.findWhere(content, {id: "recz3Mm1vkIjZxqrF"}).fields.en, es: _.findWhere(content, {id: "recz3Mm1vkIjZxqrF"}).fields.es },
+      applebooks: { en: _.findWhere(content, {id: "rec3Az6u7LqwxFLEc"}).fields.en, es: _.findWhere(content, {id: "rec3Az6u7LqwxFLEc"}).fields.es },
+
+    }
+    var data = { meta : meta, hero : hero, sections: sections, buttons: buttons, footer: footer };
+    return data;
+
+  })
+
+  /* Index */
+  const page_index = airtable.table('index').list({maxRecords:100}).then(result => {
+    var content = result.records;
+    var meta = {
+      title : { en: _.findWhere(content, {id: "recWFh4T9Cn3MWZ0a"}).fields.en, es: _.findWhere(content, {id: "recWFh4T9Cn3MWZ0a"}).fields.es },
+      description : { en: _.findWhere(content, {id: "recXNfnKxxMqlcHbA"}).fields.en, es: _.findWhere(content, {id: "recXNfnKxxMqlcHbA"}).fields.es },
+      og_image : { en: _.findWhere(content, {id: "rec08PYQ76Px2FhAG"}).fields.en, es: _.findWhere(content, {id: "rec08PYQ76Px2FhAG"}).fields.es },
+      og_url : { en: _.findWhere(content, {id: "rechdnqHLXECAYpS3"}).fields.en, es: _.findWhere(content, {id: "rechdnqHLXECAYpS3"}).fields.es },
+    }
+    var data = { meta : meta};
+    return data;
   });
+
+
+  const promiseFn = await Promise.all([elements, page_wicscn,page_index]);
+
+
+  const data = { global : promiseFn[0], wicscn : promiseFn[1],index : promiseFn[2] };
+  data.wicscn.global = promiseFn[0];
+  build('wow-i-can-see-clearly-now',data.wicscn);
+  build('index',promiseFn[2]);
+
   cb();
 
 }
@@ -130,4 +164,4 @@ function data(cb){
 exports.data = data;
 exports.build = build;
 exports.assets = assets;
-exports.default = gulp.series(assets,uielements,data);
+exports.default = gulp.series(assets,data);
